1d583e2ad372e62f6bc7a1099651b3ef7bf420dc
Brandon <bgcook@lbl.gov>
2017-03-07 12:19:59 -0800

    add missing file

---
+++ common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export PREFIX=$SCRATCH/sw_test
+#export PREFIX=/usr/common/software
+
+export TAG=`date "+%F"`
+export PKGROOT=`pwd`
+export NJOBS=10
+export TMPDIR=/tmp
+export TMPDIR=$SCRATCH/tmp
+
+# helper functions for builds
+to_lower() {
+    echo "$@" | tr '[:upper:]' '[:lower:]'
+}
+
+set_pe () {
+    PE=$1
+    TG=$2
+    curr_pe=$(to_lower "$PE_ENV")
+    curr_tg=$(to_lower "$CRAY_CPU_TARGET")
+    if [[ "$curr_pe" != "$PE" ]]; then
+        module swap PrgEnv-$curr_pe PrgEnv-$PE
+    fi
+    if [[ "$curr_tg" != "$TG" ]]; then
+        module swap craype-$curr_tg craype-$TG
+    fi
+}
